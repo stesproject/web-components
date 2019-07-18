9958fd4891a3f4bb2ae51e34e3bdbba5e627f913
Stefano Mercadante <stefano.mercadante@scaiconsulting.it>
2019-07-18 17:55:44 +0200

    lecture 5

---
+++ 2-modal-component/modal.js
@@ -0,0 +1,100 @@
+class Modal extends HTMLElement {
+  constructor() {
+    super();
+    this.isOpen = false;
+    this.attachShadow({mode: "open"});
+    this.shadowRoot.innerHTML = `
+      <style>
+        #backdrop {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100vh;
+          background: rgba(0,0,0,0.75);
+          z-index: 10;
+          opacity: 0;
+          pointer-events: none;
+        }
+
+        :host([opened]) #backdrop,
+        :host([opened]) #modal {
+          opacity: 1;
+          pointer-events: none;
+        }
+
+        #modal {
+          position: fixed;
+          top: 15vh;
+          left: 25%;
+          width: 50%;
+          z-index: 100;
+          background: white;
+          border-radius: 3px;
+          box-shadow: 0 2px 8px rgba(0,0,0,0.26);
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          opacity: 0;
+          pointer-events: none;
+        }
+
+        header {
+          padding: 1rem;
+        }
+
+        ::slotted(h1) {
+          font-size: 1.25rem;
+        }
+
+        #main {
+          padding: 1rem;
+        }
+
+        #actions {
+          border-top: 1px solid #ccc;
+          padding: 1rem;
+          display: flex;
+          justify-content: flex-end;
+        }
+
+        #actions button {
+          margin: 0 0.25rem;
+        }
+      </style>
+      <div id="backdrop"></div>
+      <div id="modal">
+        <header>
+          <slot name="title">Please Confirm Payment</slot>
+        </header>
+        <section id="main">
+          <slot></slot>
+        </section>
+        <section id="actions">
+          <button>Cancel</button>
+          <button>Okay</button>
+        </section>
+      </div>
+    `;
+
+    const slots = this.shadowRoot.querySelectorAll("slot");
+    slots[1].addEventListener("slotchange", event => {
+      console.dir(slots[1].assignedNodes());
+    });
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.isOpen = this.hasAttribute("opened");
+  }
+
+  static get observerAttributes() {
+    return ["opened"];
+  }
+
+  open() {
+    this.setAttribute("opened", "");
+    this.isOpen = true;
+  }
+}
+
+customElements.define("sp-modal", Modal);
