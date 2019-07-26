804198e6a3e35861f69e15dba1af7db5c20299e3
Stefano Mercadante <stefano.mercadante@scaiconsulting.it>
2019-07-26 14:57:50 +0200

    api

--- 3-stencil/src/components/stock-price/stock-price.tsx
+++ 3-stencil/src/components/stock-price/stock-price.tsx
@@ -1,4 +1,4 @@
-import { Component } from "@stencil/core";
+import { h, Component, State } from "@stencil/core";
 
 @Component({
   tag: "sp-stock-price",
@@ -7,5 +7,32 @@ import { Component } from "@stencil/core";
 })
 
 export class StockPrice {
+  @State() fetchedPrice : number;
 
+  onFetchStockPrice(event: Event) {
+    event.preventDefault();
+
+    fetch("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=demo")
+    .then(response => {
+      return response.json();
+    })
+    .then(parsedResponse => {
+      this.fetchedPrice = +parsedResponse["Global Quote"]["05. price"];
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+  render() {
+    return [
+      <form onSubmit={this.onFetchStockPrice.bind(this)}>
+        <input id="stock-symbol"/>
+        <button type="submit">Fetch</button>
+      </form>,
+      <div>
+        <p>Price: ${this.fetchedPrice}</p>
+      </div>
+    ];
+  }
 }
